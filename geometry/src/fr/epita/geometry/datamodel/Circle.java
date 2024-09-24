5a11d7d0da53cf1d162fea90838e66f3d164f37c
Présentation <tbr@tbr.com>
2024-09-24 15:05:04 +0200

    geometry implementation

--- geometry/src/fr/epita/geometry/datamodel/Circle.java
+++ geometry/src/fr/epita/geometry/datamodel/Circle.java
@@ -1,6 +1,6 @@
 package fr.epita.geometry.datamodel;
 
-public class Circle {
+public class Circle implements Shape{
 
     private double radius;
 
---
+++ geometry/src/fr/epita/geometry/datamodel/Shape.java
@@ -0,0 +1,8 @@
+package fr.epita.geometry.datamodel;
+
+public interface Shape {
+
+    double calculateArea();
+    double calculatePerimeter();
+
+}
