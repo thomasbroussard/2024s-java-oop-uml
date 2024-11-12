6072c52d1b9e5119f32b9dd81781173528c071ef
Présentation <tbr@tbr.com>
2024-11-12 12:46:24 +0100

    database implementation

--- db-exercises/src/fr/epita/db/Main.java
+++ db-exercises/src/fr/epita/db/Main.java
@@ -1,20 +1,40 @@
 package fr.epita.db;
 
+import fr.epita.files.datamodel.Passenger;
+
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         Connection connection = DriverManager.getConnection("jdbc:h2:mem:test", "test", "test");
-        PreparedStatement createStatement = connection.prepareStatement("CREATE TABLE USERS (name VARCHAR(255))");
+        PreparedStatement createStatement = connection.prepareStatement("CREATE TABLE PASSENGERS (" +
+                "name VARCHAR(255), " +
+                "age FLOAT, " +
+                "pclass VARCHAR(5)," +
+                "sex VARCHAR(10)," +
+                "survived INT" +
+                ")");
         createStatement.execute();
 
-        PreparedStatement insertStatement  = connection.prepareStatement("INSERT INTO USERS (name) VALUES ('test')");
-        PreparedStatement insertStatement2  = connection.prepareStatement("INSERT INTO USERS (name) VALUES ('test2')");
+        //becomes the "savePassenger()" method
+        Passenger passenger = new Passenger();
+        passenger.setName("test");
+        passenger.setSex("Male");
+        passenger.setpClass("1st");
+        passenger.setAge(35.0);
+        passenger.setSurvived(0);
+
+        PreparedStatement insertStatement  = connection.prepareStatement("INSERT INTO PASSENGERS (name, age, pclass, sex, survived) VALUES (?, ?, ?, ?, ?)");
+        insertStatement.setString(1, passenger.getName());
+        insertStatement.setDouble(2, passenger.getAge());
+        insertStatement.setString(3, passenger.getpClass());
+        insertStatement.setString(4, passenger.getSex());
+        insertStatement.setInt(5, passenger.getSurvived());
         insertStatement.execute();
-        insertStatement2.execute();
 
 
-        PreparedStatement selectQueryStatement = connection.prepareStatement("SELECT name FROM USERS");
+        //becomes the readAll() method
+        PreparedStatement selectQueryStatement = connection.prepareStatement("SELECT name FROM PASSENGERS");
         ResultSet resultSet = selectQueryStatement.executeQuery();
         while (resultSet.next()){
             System.out.println(resultSet.getString("name"));
